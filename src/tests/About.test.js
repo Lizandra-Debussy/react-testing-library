64d67bb73e83d71fd45a28dfaff977511501d9d2
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-07-19 11:37:16 -0300

    Requisito 2 concluído

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('#About: Se a página contém as informações sobre a Pokédex', () => {
+  test('Se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const aboutTitle = screen.getByRole('heading',
+      { name: /About Pokédex/i, level: 2 });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('Se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const aboutParagraph = screen.getAllByText(/pokémons/i);
+    expect(aboutParagraph.length).toBe(2);
+  });
+
+  test('Se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const aboutImage = screen.getByAltText(/pokédex/i);
+    expect(aboutImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
-// test('', () => {});
 
 describe('Se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
   test('O primeiro link deve possuir o texto Home', () => {
@@ -11,7 +9,6 @@ describe('Se o topo da aplicação contém um conjunto fixo de links de navegaç
 
     const aboutLink = screen.getByRole('link', { name: 'Home' });
     expect(aboutLink).toBeInTheDocument();
-    userEvent.click(aboutLink);
   });
 
   test('O segundo link deve possuir o texto About', () => {
@@ -19,7 +16,6 @@ describe('Se o topo da aplicação contém um conjunto fixo de links de navegaç
 
     const aboutLink = screen.getByRole('link', { name: 'About' });
     expect(aboutLink).toBeInTheDocument();
-    userEvent.click(aboutLink);
   });
 
   test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
@@ -27,6 +23,5 @@ describe('Se o topo da aplicação contém um conjunto fixo de links de navegaç
 
     const aboutLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
     expect(aboutLink).toBeInTheDocument();
-    userEvent.click(aboutLink);
   });
 });
