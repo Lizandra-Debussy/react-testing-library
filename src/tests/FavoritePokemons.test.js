14f9a3ffbb7d6818509ea01f0565fb7b677c96f4
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-07-19 16:29:48 -0300

    Requisitos 3 e 4 concluídos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('#Favorite Pokémons', () => {
+  test('Exiba na tela a msg No favorite pokemon found caso não tenha pokémons favoritos',
+    () => {
+      renderWithRouter(<App />);
+      const aboutLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      userEvent.click(aboutLink);
+
+      const aboutText = screen.getByText(/No favorite pokemon found/i);
+      expect(aboutText).toBeInTheDocument();
+    });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('#Favorite Pokémons', () => {
+  test('Se a página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe');
+
+    const notfound = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notfound).toBeInTheDocument();
+  });
+  test('se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe');
+
+    const imagePageNotFound = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(imagePageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
